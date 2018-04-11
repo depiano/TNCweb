02d9abf745e8364fee131ff5f061ed757117e8a6
Antonio De Piano <depianoantonio@gmail.com>
2018-04-11 09:35:09 +0200

    add script php

---
+++ script_tncweb/auth.php
@@ -0,0 +1,34 @@
+<?php
+session_start();
+include("function.php");
+$con=connect();
+$query="select * from amministratore where email='".$_POST['email']."' AND password='".$_POST['password']."'";
+$content='';
+$error='';
+$ris=run_query($query,$con);
+
+$er=array(
+'result' => $content,
+'error' => $error
+);
+
+if(mysql_num_rows($ris)>0)
+{
+
+	//echo "i dati ci sono\n";
+
+	$r=mysql_fetch_array($ris);
+		//echo "Risultato: ".$r."\n";
+	//$fullname=(string)$r['Fullname'];
+	//echo "Fullname: ".$fullname."\n";
+	$er['result']=$r['Fullname'];
+	$er['error']='none';
+	$_SESSION['Fullname']=$er['result'];
+	$_SESSION['Email']=$r['Email'];
+	//header('Location: ../pages/index.html');
+}
+else
+	$er['error']='yes';
+echo json_encode($er);
+close_connection($ris,$con);
+?>
---
+++ script_tncweb/funzioni.php
@@ -0,0 +1,32 @@
+<?php
+$host="localhost";
+$db="my_tcnapp";
+$us="tcnapp";
+$pas="";
+
+function connetti()
+{
+	global $us;
+	global $pas;
+	global $db;
+	global $host;
+	$con=@mysql_connect($host,$us,$pas) or die("Connessione fallita");
+	@mysql_select_db($db,$con) or die("Impossibile selezionare il database");
+	return $con;
+}
+
+function esegui_query($query,$con)
+{
+	$ris=@mysql_query($query,$con) or die("Query fallita");
+	return $ris;
+}
+
+function chiudi_connessione($ris,$con)
+{
+	if(!is_bool($ris))
+	{
+		mysql_free_result($ris);
+	}
+	mysql_close($con);
+}
+?>
